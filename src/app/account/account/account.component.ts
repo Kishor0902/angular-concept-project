83fa42f885157890182012d905bf0886ee12a1d2
Kishor Jadhav <kishor.jadhav@splus.local>
2018-07-15 14:54:04 +0530

    updated changes

--- src/app/account/account/account.component.ts
+++ src/app/account/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter,Input,Output} from '@angular/core';
 
 @Component({
   selector: 'app-account',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
+      @Input() account: {name: string, status: string};
+      @Input() id: number;
+      @Output() statusChanged = new EventEmitter<{id: number,newStatus: string}>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSet(status: string){
+    this.statusChanged.emit({id: this.id, newStatus: status });
+    console.log("Account Status has been Changed New "+ status);
   }
 
 }
